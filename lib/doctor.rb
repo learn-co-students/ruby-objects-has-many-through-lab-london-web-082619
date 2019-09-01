9856b0f9a406bb666ad3624e445e1dbf3786b4cf
github username <github email address>
2019-09-01 16:26:31 +0100

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,31 @@
+class Doctor
+
+    attr_reader :name
+@@all = []
+
+def initialize(name)
+    @name = name
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+end
+
+def appointments 
+    Appointment.all.select{|apps| apps.doctor == self}
+end
+
+def patients
+    appointments.map{|apps| apps.patient}
+end
+
+
+
+
+
+end
