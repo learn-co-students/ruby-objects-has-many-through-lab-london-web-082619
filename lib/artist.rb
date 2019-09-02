02528256bce7c141f64be837d9bb50230eccff51
github username <github email address>
2019-09-02 10:50:26 +0100

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,5 +14,22 @@ class Artist
         @@all
     end
 
+    def songs
+        Song.all.select do |song| song.artist == self
+        end
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def genres
+        songs.map do |song| song.genre
+        end
+    end
+    
+
+    
+
 
 end
